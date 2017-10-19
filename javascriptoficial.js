3be3d9032bb0cdf86e05a3d6b782c318bb2c1162
Anonymous7109 <anonymous71092003@gmail.com>
2017-10-19 15:35:41 +0100

    Add files via upload

--- javascriptoficial.js
+++ javascriptoficial.js
@@ -15,36 +15,28 @@ function changeDivs(clickedItemID){
 
 /* GAME */
 
-var tokens;
-var px = 700/5, py = 500/5; 					// space between divs
-var playArea = document.getElementById("game");
-//var remove_player = true;
+var circles; // array bidimensional of circles
 
- var Token = function (pos_x, pos_y, heap, order) {			// Create object
-        this.pos_x = pos_x;                                 // X position for CSS left property
-        this.pos_y = pos_y;                                 // Y position for CSS top property
-        this.element = document.createElement("div");       // HTML element placed in DOM
-        this.element.heap = heap;                           // Index values that correspond to the heap
-        this.element.order = order;                         // tokens array, ex tokens[heap][order]
-        //this.element.remove = remove_player(); 
+
+
+
+var Circle = function (pos_x, pos_y, heap, order) {			// Create object
+    this.pos_x = pos_x;                                 // X position for CSS left property
+    this.pos_y = pos_y;                                 // Y position for CSS top property
+    this.element = document.createElement("div");       // HTML element placed in DOM // Oject has a div propriety
+    this.heap = heap;                           // Index values that correspond to the heap // Object has a heap propriety that tells is div
+   	this.order = order;                         // tokens array, ex tokens[heap][order]	// 
 
         this.remove = function(event){
-		        console.log("[" + this.element.heap + "][" + this.element.order + "]");
+		        console.log("[" + this.heap + "][" + this.order + "]");
 
 		        // Remove the element and the object
-		        for (var j=tokens[this.element.heap].length-1 ; j >= this.element.order ; j--) {
-		        	console.log("j: " + j + " order: " + this.element.order);
-		            tokens[this.element.heap][j].element.parentNode.
-		            removeChild(tokens[this.element.heap][j].element); 		// remove the element
-		            tokens[this.element.heap].pop(); 						// remove the object
-		        }
 
-		        if (tokens[this.element.heap].length === 0) {
-		            for (var i=this.element.heap+1; i<tokens.length; i++)
-		                for (j=0; j<tokens[i].length; j++)
-		   	                tokens[i][j].element.heap--;
-		            tokens.splice(this.element.heap, 1);
-		        } 
+		        for (var j=circles[this.heap].length-1 ; j >= this.order ; j--) {  // j starts on the top of the heap and will clean until dont find the clicked position
+		            circles[this.heap][j].element.parentNode.        // an object can only be deleted by his parent
+		            removeChild(circles[this.heap][j].element); 		// remove the element
+		            circles[this.heap].pop(); 						// remove the object // remove the circle (and div) // pop method could be used in arrays
+		        }
 		} 
 
     	this.element.addEventListener("click", this.remove.bind(this), false);  
@@ -53,22 +45,22 @@ var playArea = document.getElementById("game");
 function startGame() {
         var numOfHeaps = 3; // Number of heaps in this round     
 
-        // Create a random 2D array for storing the Token objects
-        // First index represents the heap, second index represents the Token in each heap
-        tokens = Array(numOfHeaps); // create tokens[i] = nº heaps
+        // Create array for storing the Circles objects
+        // First index represents the heap, second index represents the Circles in each heap
+        circles = Array(numOfHeaps);
         for (var i=0; i<numOfHeaps; i++)
-            tokens[i] = Array(i); // create tokens[i][j] // tokens[5][5]
+            circles[i] = Array(i); 
 
         // Populate MainStage with tokens
-        for (i=0; i<tokens.length; i++) {
+        for (i=0; i<circles.length; i++) {
             for (var j=0; j<i+1; j++) {
-                tokens[i][j] = new Token(((i * px)), (720 - (py + j * py)), i, j); // 200 controls left property // 620 controls right property // i * dx and py + j * py will allow the divs with the circles to dont overlap
-                game.appendChild(tokens[i][j].element);
-                tokens[i][j].element.classList.add("circle_div");
+                circles[i][j] = new Circle(((i * 140)), (720 - (100 + j * 100)), i, j); // 200 controls left property // 620 controls right property // i * dx and py + j * py will allow the divs with the circles to dont overlap
+                game.appendChild(circles[i][j].element); // game is parentNode
+                circles[i][j].element.classList.add("circle_div"); // add className to the divs
 
                 // Specify location of each token
-                tokens[i][j].element.style.left = tokens[i][j].pos_x + "px";
-                tokens[i][j].element.style.top = tokens[i][j].pos_y + "px";
+                circles[i][j].element.style.left = circles[i][j].pos_x + "px";
+                circles[i][j].element.style.top = circles[i][j].pos_y + "px";
             }
         }
 
@@ -77,38 +69,14 @@ function startGame() {
 
 function player_move(){
 
-	/*function remove_player(event) {
-        	//console.log(remove_player);
-        	if(remove_player){
-		        console.log("[" + this.element.heap + "][" + this.element.order + "]");
-
-		        // Remove the element and the object
-		        for (var j=tokens[this.element.heap].length-1 ; j >= this.element.order ; j--) {
-		        	console.log("j: " + j + " order: " + this.element.order);
-		            tokens[this.element.heap][j].element.parentNode.
-		            removeChild(tokens[this.element.heap][j].element); 		// remove the element
-		            tokens[this.element.heap].pop(); 						// remove the object
-		        }
-
-		        if (tokens[this.element.heap].length === 0) {
-		            for (var i=this.element.heap+1; i<tokens.length; i++)
-		                for (j=0; j<tokens[i].length; j++)
-		   	                tokens[i][j].element.heap--;
-		            tokens.splice(this.element.heap, 1);
-		        } 
-		    }
-		} 
-
-    	this.element.addEventListener("click", remove_player.bind(this), false); */
-
-	if(winning_function()) alert("you WON!");
+	/*if(winning_function()) alert("you WON!");
 	else{
 		pc_move();
 		remove_player = false;
-	}
+	} */
 }
 
-function pc_move(){}
+//function pc_move(){}
 
 function winning_function(){
 	var flag = true;

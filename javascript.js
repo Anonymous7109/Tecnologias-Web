faac95f001db3f4243f4c2e13af372cab1d47f49
Anonymous7109 <anonymous71092003@gmail.com>
2017-11-28 22:48:53 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@
 
 function closeAllDivs(){
 	document.getElementById("rules").style.visibility = "hidden";
-	document.getElementById("game").style.visibility = "hidden";
+	document.getElementById("game_board").style.visibility = "hidden";
 	document.getElementById("popupLogin").style.visibility = "hidden";
 	document.getElementById("settings").style.visibility = "hidden";
 	document.getElementById("score").style.visibility = "hidden";
@@ -11,16 +11,21 @@ function closeAllDivs(){
 
 function changeDivs(clickedItemID){
 
+		//if(score_flag == 1){
+			//score_node[0].parentNode.
+			//removeChild(score_node[0]);
+		//}
+
 		closeAllDivs();
 		if(clickedItemID == 'game2'){ 
 			startGame();
-			document.getElementById('game').style.visibility = "visible";
+			document.getElementById('game_board').style.visibility = "visible";
 		}
-		else if(clickedItemID == 'game' && gameISover == 1){
+		else if(clickedItemID == 'game_board' && gameISover == 1){
 			startGame();
 			document.getElementById(clickedItemID).style.visibility = "visible";
 		}
-		else if(clickedItemID == 'game') document.getElementById(clickedItemID).style.visibility = "visible";
+		else if(clickedItemID == 'game_board') document.getElementById(clickedItemID).style.visibility = "visible";
 		else document.getElementById(clickedItemID).style.visibility = "visible";
 }
 
@@ -32,12 +37,21 @@ var pc_remove;
 var who_plays = 0;
 var numOfColumns = 3; // Number of columns set by the user
 var dificulty = 0;
+var online = false;
+var game_value;
+var LoginObj = {};
 
 var matrix = Array(numOfColumns);
 var array = Array(numOfColumns);
 var clicked = Array(numOfColumns); // clicked = [[,,,,],[,,,,],[,,,,]]
 //var game = document.getElementById("game");
 var gameISover;
+var score = Array(3);
+var score_flag = 0;
+
+for ( i = 0 ; i < 3 ; i++) score[i] = 0;
+
+var score_node = Array(1);
 
 function init_arrays(){
 	var i, j;
@@ -57,6 +71,31 @@ var Circle = function (pos_x, pos_y, column, order) {		   // Create object
     this.element = document.createElement("div");     	  // HTML element placed in DOM // Oject has a div propriety
     this.column = column;                          			  // Index values that correspond to the column // Object has a column propriety that tells is div
    	this.order = order;                        			 // circles array, ex circles[column][order]	// 
+}
+
+function startGame_online(who_starts_playing){
+	var i, j;
+
+	circles = Array(numOfColumns);
+        for (j=0; j<numOfColumns; j++)
+            circles[j] = Array(j); 
+
+    init_arrays();
+
+    // Populate MainStage with circles
+    for (i=0; i<circles.length; i++) {
+        for (j=0; j<i+1; j++) {
+            circles[i][j] = new Circle(((i * 140)), (720 - (100 + j * 100)), i, j); // first 100 controlos bottom property // second 100 controls left // 140 controls right // 720 controls top property // i * dx and py + j * py will allow the divs with the circles to dont overlap
+            game_board.appendChild(circles[i][j].element); // game is parentNode
+            circles[i][j].element.classList.add("circle_div"); // add className to the divs
+            circles[i][j].element.addEventListener("click", playerremove.bind(circles[i][j])); /* circles[i][j].element.setAtribute("onclick", "remove();"); */
+            // Specify location of each circle
+            circles[i][j].element.style.left = circles[i][j].pos_x + "px";
+            circles[i][j].element.style.top = circles[i][j].pos_y + "px";
+        }
+    }
+
+    playerremove_online(who_starts_playing);
 }
 
 function startGame() {  
@@ -89,7 +128,7 @@ function startGame() {
         for (i=0; i<circles.length; i++) {
             for (j=0; j<i+1; j++) {
                 circles[i][j] = new Circle(((i * 140)), (720 - (100 + j * 100)), i, j); // first 100 controlos bottom property // second 100 controls left // 140 controls right // 720 controls top property // i * dx and py + j * py will allow the divs with the circles to dont overlap
-                game.appendChild(circles[i][j].element); // game is parentNode
+                game_board.appendChild(circles[i][j].element); // game is parentNode
                 circles[i][j].element.classList.add("circle_div"); // add className to the divs
                 circles[i][j].element.addEventListener("click", playerremove.bind(circles[i][j])); /* circles[i][j].element.setAtribute("onclick", "remove();"); */
                 // Specify location of each circle
@@ -101,6 +140,12 @@ function startGame() {
     if(pc_remove) pc_move();
 }
 
+
+function playerremove_online(who_starts_playing){
+	if(who_starts_playing == LoginObj.nick){
+		
+	}
+}
 
 function playerremove(){
 	//   console.log("[" + this.column + "][" + this.order + "]");
@@ -118,6 +163,9 @@ function playerremove(){
 
 		if(winning_function()){ 
 			alert("Player You Won!");
+			if(dificulty == 0) score[0]++;
+			else if(dificulty == 1) score[1]++;
+			else score[2]++;
 			gameISover = 1;
 			closeAllDivs();
 			flag = false;
@@ -198,13 +246,6 @@ function pc_move(){
 				it++;
 			}
 		}
-
-		/*for(i = 0 ; i < numOfColumns ; i++){
-			if((sum[i] % 2) != 0){
-				odd_index[it] = i;
-				it++;
-			}
-		} */
 
 		for(i = 0 ; i < odd_index.length ; i++) console.log("i: " + i + " odd_index[i]: " + odd_index[i]);
 
@@ -292,14 +333,145 @@ function winning_function(){
 }
 
 function login(){
-	console.log(document.getElementById('username').value);
-	console.log(document.getElementById('password').value);
+	console.log(login);
+	var nick = document.getElementById('username').value;
+	var pass = document.getElementById('password').value;
+	var string = new String("{}");
+
+	if(pass != "" && nick != ""){
+		LoginObj.nick = nick;
+		LoginObj.pass = pass;
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST","http://twserver.alunos.dcc.fc.up.pt:8008/register", true);
+		xhr.onreadystatechange = function(){
+			if(xhr.readstate < 4) return;
+			if(xhr.status == 200){
+				var data = JSON.parse(xhr.responseText);
+				if(!(data == string)){
+					alert(data);
+					console.log("login_done")
+					online = true;
+				}
+			}
+			else{
+				alert(xhr.status);
+			}
+		}
+		xhr.send(JSON.stringify(LoginObj));
+	}
+
+	
+
+	/*
+
+	LoginObj = {"nick":"\" + nick + \"", "password":"" + pass + ""}
+
+	//var print = JSON.parse(LoginObj);
+	console.log(LoginObj);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://twserver.alunos.dcc.fc.up.pt:8008/register", true);
+	xhr.onreadystatechange = function(){
+		console.log("readystate-login");
+		if(xhr.readstate < 4) return;
+		if(xhr.status == 200){
+			var data = JSON.parse(xhr.responseText);
+			if(!(data == string)){
+				alert(data);
+				console.log("login_done")
+				online = true;
+			}
+		}
+		else{
+			alert(xhr.status);
+		}
+	}
+	xhr.send(LoginObj); */
 }
 
-function closeLogin() {
+function closeLogin(){
 	closeAllDivs();
     document.getElementById('username').value = "";
     document.getElementById('password').value = "";
+}
+
+function join_match(){
+	//online = true; // DEBUG
+	if(online){
+		JoinObj = { "group": 49, "nick": "" + LoginObj.nick + "", "pass": "" + LoginObj.pass + "", "size": numOfColumns }
+		// DEBUG:
+		//JoinObj = { "group": 49, "nick": "andre", "pass": "123", "size": numOfColumns }
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://twserver.alunos.dcc.fc.up.pt:8008/join", true);
+		xhr.onreadystatechange = function(){
+			console.log("readystate-join");
+			if(xhr.readstate < 4) return;
+			if(xhr.status == 200){
+				var data = JSON.parse(xhr.responseText);
+				game_value = data.game;
+				console.log("game_value: " + game_value);
+			}
+			else{
+				alert(xhr.status);
+			}
+		}
+		xhr.send(JoinObj);
+
+		update();
+	}
+	else alert("You need to login first")
+}
+
+function leave(){
+	if(online){
+		LeaveObj = { "nick": "" + LoginObj.nick + "", "pass": "" + LoginObj.pass + "", "game": "" + game_value + "" }
+		var string = new String("{}");
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://twserver.alunos.dcc.fc.up.pt:8008/leave", true);
+		xhr.onreadystatechange = function(){
+			console.log("readysstate-leave");
+			if(xhr.readstate < 4) return;
+			if(xhr.status == 200){
+				var data = JSON.parse(xhr.responseText);
+				if(!(data == string)){
+					alert(data);
+					eventSource.close();
+				}
+			}
+			else{
+				alert(xhr.status);
+			}
+		}
+		xhr.send(LoginObj);
+	}
+	else{}
+}
+
+function notify(){
+}
+
+function update(){
+	// server event // espera que encontra jogo // Se encontrar jogo -> imagem no canvas -> aparece o tabuleiro // e comecam a jogar usando a funçao notify sempre que uma jogado for feita
+	var url = "http://twserver.alunos.dcc.fc.up.pt:8008/update?nick="+LoginObj.nick+"&game="+game_value+"";
+	var string = new String("{}");
+
+	var eventSource = new EventSource("url");
+
+	if(eventSource.readstate == 1) console.log("eventSource OPEN");
+
+	eventSource.onmessage = function(event){
+		var data = JSON.parse(event.data);
+		if(data != string){
+			// CANVAS GAME STARTED
+			var who_starts_playing = data.turn;
+			console.log("who starts playing " + who_starts_playing); 
+			console.log("online game started");
+			startGame_online(who_starts_playing);
+		}
+	}
 }
 
 function send_configs(){
@@ -349,4 +521,53 @@ function send_configs(){
 
 function isInArray(value, array) {
   return array.indexOf(value) > -1;
+}
+
+function ScoreBoard(){
+	score_flag = 1;
+
+		/*if(document.getElementById('offline').selected){
+			if(document.getElementById('easy_option').selected) var text = document.createTextNode("#1" + " localhost " + score[0] + " easy");
+			else if(document.getElementById('normal_option').selected) var text = document.createTextNode("#1" + " localhost " + score[1] + " normal");
+			else if(document.getElementById('hardest_option').selected) var text = document.createTextNode("#1" + " localhost " + score[2] + " hardest");
+			score_node[0] = document.createElement("div");
+			score_node[0].appendChild(text);
+			document.getElementById("body_scores").appendChild(score_node[0]);
+			//score_node.style.width = "200px";
+			//score_node.style.display = "inline";
+    	}
+ else */if(document.getElementById('online').selected){
+ 			var o = document.getElementById("Score_GameLevel");
+			var size_value = o.options[o.selectedIndex].value;
+    		console.log(size_value);
+    		rankObj = { "size": "" + size_value + "" }
+			var string = new String("{}");
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", "http://twserver.alunos.dcc.fc.up.pt:8008/ranking", true);
+			xhr.onreadystatechange = function(){
+				console.log("readysstate-leave");
+				if(xhr.readstate < 4) return;
+				if(xhr.status == 200){
+					var data = JSON.parse(xhr.responseText);
+					if(data == string){
+						alert("Ainda sem tabela classificativa");
+					}
+					else{
+						for(var i = 0 ; i < data.length ; i++){
+							var cont = 0;
+							var text = document.createTextNode("#" + cont + " " + data.ranking[i].nick + " " + data.ranking[i].victories + " " + data.ranking[i].games);
+							score_node[0] = document.createElement("div");
+							score_node[0].appendChild(text);
+							document.getElementById("body_scores").appendChild(score_node[0]);
+							cont++;
+						}
+					}
+				}
+				else{
+					alert(xhr.status);
+				}
+			}
+			xhr.send(rankObj);
+    	}
 }
